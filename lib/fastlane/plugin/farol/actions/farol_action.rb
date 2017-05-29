b757a91d0c9fda6dca6f3bad4c547a05f7201d63
Felipe Plets <felipe.plets@menvia.com>
2017-05-28 22:15:18 -0300

    Add Farol get and set version methods

--- lib/fastlane/plugin/farol/actions/farol_action.rb
+++ lib/fastlane/plugin/farol/actions/farol_action.rb
@@ -2,7 +2,50 @@ module Fastlane
   module Actions
     class FarolAction < Action
       def self.run(params)
-        UI.message("The farol plugin is working!")
+        baseFolder 		= ''
+        fasFolder 		= baseFolder + '.fas/'
+        targetFolder 	= baseFolder + 'Target/'
+
+        # Reset target folder
+      	FileUtils.rm_rf Dir.glob(targetFolder)
+      	Dir.mkdir(targetFolder)
+
+        result = self.request(params, 'get', 'event/event')
+        self.saveFile('Event', result)
+
+        result = self.request(params, 'get', 'app/' + params[:app_id])
+        self.saveFile('App', result)
+
+      end
+
+      def self.request(params, verb, method)
+        farolURL = 'https://frl.io:443'
+
+        # Create Cognito credentials
+        url = URI(farolURL + '/' + method)
+        Net::HTTP.start(url.host, url.port,
+          :use_ssl => url.scheme == 'https') do |http|
+
+          if verb == 'post'
+            request = Net::HTTP::Post.new url
+          elsif verb == 'put'
+            request = Net::HTTP::Put.new url
+          else
+            request = Net::HTTP::Get.new url
+          end
+
+          request['authorization'] = params[:token]
+          response = http.request request # Net::HTTPResponse object
+          result = JSON.parse response.read_body
+        end
+      end
+
+      def self.saveFile(fileName, result)
+        baseFolder 		= ''
+        targetFolder 	= baseFolder + 'Target/'
+        File.open(targetFolder + fileName + '.json', 'w') do |f|
+          f.puts JSON.pretty_generate(result)
+        end
       end
 
       def self.description
@@ -24,11 +67,16 @@ module Fastlane
 
       def self.available_options
         [
-          # FastlaneCore::ConfigItem.new(key: :your_option,
-          #                         env_name: "FAROL_YOUR_OPTION",
-          #                      description: "A description of your option",
-          #                         optional: false,
-          #                             type: String)
+          FastlaneCore::ConfigItem.new(key: :token,
+                                  env_name: "TOKEN",
+                               description: "The token for your Farol App",
+                                  optional: false,
+                                      type: String),
+          FastlaneCore::ConfigItem.new(key: :app_id,
+                                  env_name: "APP_ID",
+                               description: "The app id for your Farol App",
+                                  optional: false,
+                                      type: String)
         ]
       end
 
